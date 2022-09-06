1b4863c17fb99e0586b8341f85474f8b7fd9ccaa
Kelvin Brito <britok30@gmail.com>
2022-09-06 12:24:49 -0400

    seo improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,16 +60,25 @@ const Home = ({ randomColor }: { randomColor: RgbColor }) => {
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="utf-8" />
         <meta content="Kelvin Brito" name="author" />
-        <meta property="og:type" content="website" />
         <meta name="theme-color" content={hex} />
         <meta
           name="keywords"
-          content="color picker, hex color picker, rgb color picker, color conversions, hex, colour"
+          content="color picker, hex color picker, rgb color picker, color conversions, hex, colour, color hue picker"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
+
         <meta property="og:title" content={pageTitle} key="ogtitle" />
         <meta property="og:description" content={description} key="ogdesc" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://www.colorhuepicker.com/"
+          key="ogurl"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={description} />
       </Head>
 
       <main className="flex flex-col space-y-5 items-center justify-center">
