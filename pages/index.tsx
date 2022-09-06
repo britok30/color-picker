c06e35f3e50ddb2006d9230f7c2ad4eb72771b1e
Kelvin Brito <britok30@gmail.com>
2022-09-06 12:08:52 -0400

    comment update

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ const Home = ({ randomColor }: { randomColor: RgbColor }) => {
   };
 
   useEffect(() => {
-    // Generate randoem color
+    // Set random color
     setColor(randomColor);
   }, []);
 
