5d4a7be49201fe4605972f8588533087c516cfbd
Kelvin Brito <britok30@gmail.com>
2022-09-06 12:52:57 -0400

    footer updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,14 +12,32 @@ export const Footer = () => {
   }, []);
 
   return (
-    <footer className="block w-full text-xs text-center my-14 font-light">
+    <footer className="block w-[260px] text-xs text-center my-14 font-light">
       <p>
         Made with
         <span className="mx-2" role="img" aria-label="emoji">
           ❤️
         </span>
-        with React, Next.js, TypeScript & Tailwind
+        with React, Next.js, TypeScript, Tailwind,{" "}
+        <a
+          className="hover:underline"
+          href="https://github.com/omgovich/react-colorful"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Colorful
+        </a>{" "}
+        &{" "}
+        <a
+          className="hover:underline"
+          href="https://github.com/omgovich/colord"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Colord
+        </a>
       </p>
+
       <p>&copy; {date}</p>
     </footer>
   );
